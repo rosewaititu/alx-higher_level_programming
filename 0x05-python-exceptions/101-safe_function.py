6f51584120ec36abbd90b36d953c2e8164d4038b
rosewaititu <rosewaithera2014@gmail.com>
2023-01-24 10:39:56 -0800

    task

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-def safe_function(fct, *args):
+import sys
 
+
+def safe_print_integer_err(value):
     try:
-        return fct(*args)
-    except Exception as exception:
-        print('Exception: {}'.format(exception), file=sys.stderr)
+        print('{:d}'.format(value))
+    except Exception as error:
+        print('Exception: {}'.format(error), file=sys.stderr)
+        return False
 
+    return True
